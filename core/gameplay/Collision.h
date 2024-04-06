a7aae1e675500dfd0b12fa1313545ff3502547d2
SenneW-2158088 <senne.wertelaers@student.uhasselt.be>
2024-04-06 13:22:26 +0200

    Implemented AABB

--- core/gameplay/Collision.h
+++ core/gameplay/Collision.h
@@ -1,33 +1,44 @@
 #pragma once
 
+#include "graphics/Mesh.h"
 #include "model/Entity.h"
 #include <glm/glm.hpp>
+#include <optional>
 
 namespace Collision {
 class AABoundingBox;
 
 class BoundingBox {
+public:
   virtual bool collides(BoundingBox &other);
-  virtual void collideWith(AABoundingBox &other){};
+  virtual bool collideWith(AABoundingBox &other) { return false; };
 };
 
-class AABoundingBox : BoundingBox {
+class AABoundingBox : public BoundingBox {
 private:
   glm::vec3 min, max;
-
 public:
+  AABoundingBox(glm::vec3 min, glm::vec3 max);
+  AABoundingBox(Mesh::Mesh* mesh);
   bool collides(BoundingBox &other) override;
-  void collideWith(AABoundingBox &other) override;
+  bool collideWith(AABoundingBox &other) override;
 
 public:
   glm::vec3 getMin() const { return min; };
-  glm::vec3 getMax() const { return max;};
+  glm::vec3 getMax() const { return max; };
 };
 
 class Collisioner {
 private:
-  Entity *entity;
+  std::optional<Entity *> entity;
   BoundingBox boundingbox;
+
+public:
+  std::optional<Entity *> getEntity() const { return entity; };
+  BoundingBox *getBoundingBox() {return &boundingbox;};
+
+public:
+  Collisioner();
 };
 
 class Collisionable {
